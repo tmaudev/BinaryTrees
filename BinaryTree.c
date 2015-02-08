b80133db54b3cc0b0b666ac8e2f137504eaa1b1a
Tyler Mau <tmau@calpoly.edu>
2015-02-07 19:13:30 -0800

    Working Implementation

--- BinaryTree.c
+++ BinaryTree.c
@@ -10,17 +10,20 @@ typedef struct Node {
    struct Node *right;
 }Node;
 
+/* Queue Node Struct */
 typedef struct QueueNode {
    Node *node;
    struct QueueNode *next;
    struct QueueNode *prev;
 }QueueNode;
 
+/* Queue Struct */
 typedef struct Queue {
    QueueNode *head;
    QueueNode *tail;
 }Queue;
 
+/* Add to Tail of Queue */
 void enqueue(Queue *queue, Node *node) {
    QueueNode *new = calloc(1, sizeof(struct QueueNode));
 
@@ -37,6 +40,7 @@ void enqueue(Queue *queue, Node *node) {
    }
 }
 
+/* Remove from Head of Queue */
 Node *dequeue(Queue *queue) {
    Node *node;
    QueueNode *temp = (*queue).head;
@@ -70,6 +74,98 @@ Node *recurInsert(Node *node, int elem) {
 
 void insert(Node **root, int elem) {
    *root = recurInsert(*root, elem);
+}
+
+/* Return Number of Nodes in Tree */
+int size(Node *node) {
+   if (!node) {
+      return 0;
+   }
+
+   return size(node->left) + 1 + size(node->right);
+}
+
+Node *findMin(Node *node) {
+   if (!node) {
+      return NULL;
+   }
+
+   if (node->left) {
+      return findMin(node->left);
+   }
+   else {
+      return node;
+   }
+}
+
+Node *findMax(Node *node) {
+   if (!node) {
+      return NULL;
+   }
+
+   if (node->right) {
+      return findMin(node->right);
+   }
+   else {
+      return node;
+   }
+}
+
+/* Return Node Containing Element */
+Node *find(Node *node, int elem) {
+   if (!node) {
+      return NULL;
+   }
+
+   if (elem > node->data) {
+      return find(node->right, elem);
+   }
+   else if (elem < node->data) {
+      return find(node->left, elem);
+   }
+   else {
+      return node;
+   }
+}
+
+Node *deleteNode(Node *node, int elem) {
+   Node *temp;
+
+   if (!node) {
+      return node;
+   }
+
+   if (elem < node->data) {
+      node->left = deleteNode(node->left, elem);
+   }
+   else if (elem > node->data) {
+      node->right = deleteNode(node->right, elem);
+   }
+   else {
+      /* One or No Child */
+      if (!node->left) {
+         temp = node->right;
+         free(node);
+         return temp;
+      }
+      else if (!node->right) {
+         temp = node->left;
+         free(node);
+         return temp;
+      }
+
+      /* Two Children */
+      temp = findMin(node->right);
+      node->data = temp->data;
+      node->right = deleteNode(node->right, temp->data);
+   }
+   return node;
+}
+
+void deleteItem(Node **root, int elem) {
+   if (*root) {
+      *root = deleteNode(*root, elem);
+   }
 }
 
 /* Print Breadth First */
@@ -107,7 +203,7 @@ void printInOrder(Node *node) {
 
 void printPreOrder(Node *node) {
    if (node) {
-      print("%d ", node->data);
+      printf("%d ", node->data);
       printPreOrder(node->left);
       printPreOrder(node->right);
    }
@@ -117,8 +213,52 @@ void printPostOrder(Node *node) {
    if (node) {
       printPostOrder(node->left);
       printPostOrder(node->right);
-      print("%d ", node->data);
+      printf("%d ", node->data);
    }
+}
+
+int maxDepth(Node *node) {
+   int leftDepth, rightDepth;
+
+   if (!node) {
+      return 0;
+   }
+
+   leftDepth = maxDepth(node->left);
+   rightDepth = maxDepth(node->right);
+
+   if (leftDepth > rightDepth) {
+      return leftDepth + 1;
+   }
+   else {
+      return rightDepth + 1;
+   }
+}
+
+int getLevel(Node *node, int data, int level) {
+   int height;
+
+   /* Return 0 If Not Found */
+   if (!node) {
+      return 0;
+   }
+
+   /* If Found, Return Level */
+   if (node->data == data) {
+      return level;
+   }
+
+   /* Search Left Subtree */
+   if ((height = getLevel(node->left, data, level + 1))) {
+      return height;
+   }
+
+   /* Search Right Subtree */
+   return getLevel(node->right, data, level + 1);
+}
+
+int nodeLevel(Node* node, int data) {
+   return getLevel(node, data, 1);
 }
 
 /* Testing */
@@ -132,6 +272,15 @@ int main() {
    }
 
    printLevelOrder(root);
+   printf("\n");
+
+   deleteItem(&root, 5);
+
+   printLevelOrder(root);
+   printf("\n");
+
+   printf("Max Depth: %d\n", maxDepth(root));
+   printf("Level of 8: %d\n", nodeLevel(root, 8));
 
    return 0;
 }
